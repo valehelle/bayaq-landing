42fc19d2fd2248cc3156877dad1d3de84bb8b895
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2020-07-09 14:49:34 +0800

    add GA

--- pages/android.js
+++ pages/android.js
@@ -1,11 +1,32 @@
 import Head from 'next/head'
 import Button from 'react-bootstrap/Button';
 import { View, TextInput, TouchableOpacity, Image, Text, Linking } from 'react-native-web'
+import { useEffect } from 'react';
 
 const Android = () => {
-  Linking.openURL('https://play.google.com/store/apps/details?id=com.bayaqapp.www')
+
+  useEffect(() => {
+    setTimeout(function () { Linking.openURL('https://play.google.com/store/apps/details?id=com.bayaqapp.www') }, 2000)
+
+
+  }, [])
   return (
-    <></>
+    <Head>
+      <title>Bayaq</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115169227-7"></script>
+      <script>{`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-115169227-7');
+  
+`}
+
+      </script>
+    </Head>
   )
 }
 
--- pages/ios.js
+++ pages/ios.js
@@ -1,11 +1,31 @@
 import Head from 'next/head'
 import Button from 'react-bootstrap/Button';
 import { View, TextInput, TouchableOpacity, Image, Text, Linking } from 'react-native-web'
+import { useEffect } from 'react';
 
 const Ios = () => {
-  Linking.openURL('https://apps.apple.com/my/app/bayaq/id1519715626')
+  useEffect(() => {
+    setTimeout(function () { Linking.openURL('https://apps.apple.com/my/app/bayaq/id1519715626') }, 2000)
+
+
+  }, [])
   return (
-    <></>
+    <Head>
+      <title>Bayaq</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115169227-7"></script>
+      <script>{`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-115169227-7');
+  
+`}
+
+      </script>
+    </Head>
   )
 }
 
